e32ec18f3e64ada27f9a80f24c7079ce6747a45c
RahulSSM <rk0391@gmail.com>
2018-08-29 09:00:36 +0530

    Migration script updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,15 +12,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-//        Schema::create('users', function (Blueprint $table) {
-//            $table->increments('id');
-//            $table->string('name');
-//            $table->string('email')->unique();
-//            $table->string('password');
-//            $table->string('api_token',100)->unique();
-//            $table->rememberToken();
-//            $table->timestamps();
-//        });
+       Schema::create('users', function (Blueprint $table) {
+           $table->increments('id');
+           $table->string('name');
+           $table->string('email')->unique();
+           $table->string('password');
+           $table->string('api_token',100)->unique();
+           $table->rememberToken();
+           $table->timestamps();
+       });
     }
 
     /**
